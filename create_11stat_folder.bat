0bc41738c9bdca63dd26d7f337d38a599ddc76f5
Wenchen <chenwen_23@163.com>
2025-01-28 22:56:05 +0800

    Add files via upload

---
+++ create_11stat_folder.bat
@@ -0,0 +1,31 @@
+@echo off
+
+REM 创建一级文件夹及其子文件夹
+mkdir "11Statistics\11.01Statistics_Oversight\11.01.01Statistical_Analysis_Plan"
+mkdir "11Statistics\11.01Statistics_Oversight\11.01.02Sample_Size_Calculation"
+mkdir "11Statistics\11.02Randomization\11.02.01Randomization_Plan"
+mkdir "11Statistics\11.02Randomization\11.02.02Randomization_Procedure"
+mkdir "11Statistics\11.02Randomization\11.02.03Master_Randomization_List"
+mkdir "11Statistics\11.02Randomization\11.02.04Randomization_Programming"
+mkdir "11Statistics\11.02Randomization\11.02.05Randomization_Sign_Off"
+mkdir "11Statistics\11.02Randomization\11.02.06End_of_Trial_or_Interim_Unblinding"
+mkdir "11Statistics\11.03Analysis\11.03.01Data_Definitions_for_Analysis_Datasets"
+mkdir "11Statistics\11.03Analysis\11.03.02Analysis_QC_Documentation"
+mkdir "11Statistics\11.03Analysis\11.03.03Interim_Analysis_Raw_Datasets"
+mkdir "11Statistics\11.03Analysis\11.03.04Interim_Analysis_Programs"
+mkdir "11Statistics\11.03Analysis\11.03.05Interim_Analysis_Datasets"
+mkdir "11Statistics\11.03Analysis\11.03.06Interim_Analysis_Output"
+mkdir "11Statistics\11.03Analysis\11.03.07Final_Analysis_Raw_Datasets"
+mkdir "11Statistics\11.03Analysis\11.03.08Final_Analysis_Programs"
+mkdir "11Statistics\11.03Analysis\11.03.09Final_Analysis_Datasets"
+mkdir "11Statistics\11.03Analysis\11.03.10Final_Analysis_Output"
+mkdir "11Statistics\11.03Analysis\11.03.11Subject_Evaluability_Criteria_and_Subject_Classification"
+mkdir "11Statistics\11.04Report\11.04.01Interim_Statistical_Report(s)"
+mkdir "11Statistics\11.04Report\11.04.02Statistical_Report"
+mkdir "11Statistics\11.05General\11.05.01Relevant_Communications"
+mkdir "11Statistics\11.05General\11.05.02Tracking_Information"
+mkdir "11Statistics\11.05General\11.05.03Meeting_Material"
+mkdir "11Statistics\11.05General\11.05.04Filenote"
+
+echo 所有文件夹已创建完毕。
+pause
